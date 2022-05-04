67cfc23a3d9f7268b6672921885e196da974039c
Mark Karp <mark@karp.co>
2022-05-03 22:04:14 -0400

    new files

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Gpv9
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
+    # mk added
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
